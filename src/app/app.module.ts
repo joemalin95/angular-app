e2bfc0b572dc21986b438eaee95c4765bb47cef9
Joey Malinowski <joemalin95@gmail.com>
2018-04-30 08:10:25 +0000

    initialized and customized shell from template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+import { SidebarModule } from './sidebar/sidebar.module';
+import { NavbarModule} from './navbar/navbar.module';
+import { NguiMapModule} from '@ngui/map';
 
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { AssignComponent } from './assign/assign.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AssignComponent,
+    ReviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    SidebarModule,
+    NavbarModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyBk40AU-WAyeI8O7xjgpXKeJkvOMbwSse0'})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
