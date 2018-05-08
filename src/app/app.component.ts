cc6d313993c244532834b1271ead890541c46524
Joey Malinowski <joemalin95@gmail.com>
2018-05-08 06:01:32 +0000

    Revert "changes"
    This reverts commit 2329d7e89552662236a20491534397e258172871.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
-  <button (click)="AddBook()">Add a book!</button>
 })
 
-export class AppComponent{
-
-private bookCounter = 0;
-
-    public AddBook(): void {
-        let newBook = new Book(`My book #${this.bookCounter++}`);
-        this.books.push(newBook);
-    }
-
-}
+export class AppComponent{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,33 +12,6 @@ import { DashboardComponent }   from './dashboard/dashboard.component';
 import { AssignComponent } from './assign/assign.component';
 import { ReviewComponent } from './review/review.component';
 
-
-import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-
-class Book {
-    constructor(public title) { }
-}
-
-@Component({
-    selector: 'app-root',
-    template: `
-        <ul>
-            <li *ngFor="let book of books | async">
-                <pre>{{ book | json }}</pre>
-            </li>
-        </ul>
-    `
-})
-export class AppComponent {
-    public books: FirebaseListObservable<Book[]>;
-    constructor(db: AngularFireDatabase) {
-        this.books = db.list('/books');
-    }
-}
 @NgModule({
   declarations: [
     AppComponent,
@@ -48,8 +21,6 @@ export class AppComponent {
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase, 'something'),
-        AngularFireDatabaseModule
     RouterModule.forRoot(AppRoutes),
     SidebarModule,
     NavbarModule,
